2c3c29bc6130a60d2a8fd2dbac65d253374c549b
joaopfusco <125307931+joaopfusco@users.noreply.github.com>
2024-03-18 21:36:49 -0300

    adicionando condições para deploy
     hospedagem na vercel e arquivos estaticos

--- termoproject/settings.py
+++ termoproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-5^vvp=&%5r1191nnbvo@3mt@fqz+)yw$1do8mpao^d8mcoydvh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- termoproject/urls.py
+++ termoproject/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include, reverse_lazy
 from django.views.generic.base import RedirectView
+from termoproject import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', RedirectView.as_view(url=reverse_lazy('termoapp:home'))),
     path('admin/', admin.site.urls),
     path('termo/', include('termoapp.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
